a90486e3dc86c8071c1421f5d78e600ceff4a51a
Surya Basnet <142815031+suryaBdrBasnet@users.noreply.github.com>
2023-10-01 21:32:52 +0545

    update done

--- script.js
+++ script.js
@@ -5,31 +5,35 @@ const resetButton = document.getElementById('reset');
 
 let startTime = 0;
 let intervalId = null;
-
+let pausedTime = 0;
 
 function formatTime(ms) {
     const hours = Math.floor(ms / 3600000);
-    const minutes = Math.floor((ms % 3600000) / 60000); // (ms % 1000*60*60) / 1000*60
+    const minutes = Math.floor((ms % 3600000) / 60000);
     const seconds = Math.floor((ms % 60000) / 1000);
+    const milliseconds = Math.floor(ms % 1000);
 
-    const formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+    const formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}:${String(milliseconds).padStart(3, '0')}`;
     // padStart(length, valueIfNotEqualTolength) -> here if hour/min/sec is <10, then it'll be 0(0-9).
     return formattedTime;
 }
 
 function updateTime() {
-    const currentTime = Date.now() - startTime; // Date.now provides currentTimestamp in term of millisecond
+    const currentTime = Date.now() - startTime + pausedTime;
+    // Date.now provides currentTimestamp in term of millisecond
     timeElement.textContent = formatTime(currentTime);
 }
 
 startButton.addEventListener('click', () => {
     if (!intervalId) {
-        startTime = Date.now() - (intervalId ? (Date.now() - startTime) : 0);
-        intervalId = setInterval(updateTime, 1000);
+        startTime = pausedTime > 0? (Date.now() - pausedTime) : Date.now();
+        pausedTime = 0;
+        intervalId = setInterval(updateTime, 1);
     }
 });
 
 stopButton.addEventListener('click', () => {
+    pausedTime = Date.now() - startTime;
     clearInterval(intervalId);
     intervalId = null;
 });
@@ -37,6 +41,7 @@ stopButton.addEventListener('click', () => {
 resetButton.addEventListener('click', () => {
     clearInterval(intervalId);
     intervalId = null;
+    pausedTime= 0;
     startTime = 0;
-    timeElement.textContent = '00:00:00';
-});
+    timeElement.textContent = '00:00:00:000';
+})
